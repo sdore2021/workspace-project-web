ac9e5fd4d0163842644038cabee431a7e437bfbe
s. dore <samouka.dore@insa-cvl.fr>
2020-03-09 15:20:43 +0100

    ddjdjdj

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
-import React, { Component } from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import Navbar from './components/NavBar'
-import Home from './components/Home'
-import Cart from './components/Cart'
+import React, { Component } from "react";
+import "./App.css";
+//import axios from "axios";
+//import { Router } from "react-router-dom";
+import Articles from "./components/articles";
+import Clients from "./components/clients";
+import Commandes from "./components/commades";
+import InsertArticle from "./components/insertAticle";
+import AdminHome from "./components/adminHome";
+import ModalArticle from "./components/modalArticle";
 
 class App extends Component {
   render() {
-    return (
-       <BrowserRouter>
-            <div className="App">
-            
-              <Navbar/>
-                <Switch>
-                    <Route exact path="/" component={Home}/>
-                    <Route path="/cart" component={Cart}/>
-                  </Switch>
-             </div>
-       </BrowserRouter>
-      
-    );
+    return <Articles />;
   }
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-
-import App from './App';
-import cartReducer from './reducers/cartReducer';
-// import Login from './components/Login';
-
-const store = createStore(cartReducer);
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <Provider store={store}>
-    
-    <App />  
-  </Provider>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
